f5c8c84093d18e1c1c43f122cc25f3963387f22f
Kyung <hkjlee109@gmail.com>
2025-08-31 22:10:14 +0200

    Create core package.

---
+++ packages/core/dist/Button.js
@@ -0,0 +1,6 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+export var Button = function (_a) {
+    var label = _a.label;
+    return _jsx("button", { children: label });
+};
+//# sourceMappingURL=Button.js.map
--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
+import { Button } from '@react-drag-drop/core'
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +9,7 @@ function App() {
   return (
     <>
       <h1>Playground</h1>
+      <Button label="OK" />
     </>
   )
 }
